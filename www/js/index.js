c071f54a2a2c8e6dc572b7cbcd460e6bd4c30563
jasalguero <jose.salguero@groupon.com>
2013-07-23 23:37:52 +0200

    trying to fix the mobile

--- www/js/index.js
+++ www/js/index.js
@@ -20,17 +20,18 @@ var app = {
 
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
-//        this.showAlert("app initialised", "hey!");
+//        this.bindEvents();
+        this.showAlert("app initialised", "hey!");
+        var map = L.mapbox.map('map', 'jasalguero.map-5tj37px7');
 
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+//    bindEvents: function() {
+//        document.addEventListener('deviceready', this.onDeviceReady, false);
+//    },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
